1bc83da6f2751a2d1900a2fa95a6af970a0f68fd
Hailagaz <omentielvo.lumenn@gmail.com>
2022-11-01 19:11:19 +0200

    Add slider1 to section projects

--- js/sectionProjects.js
+++ js/sectionProjects.js
@@ -17,3 +17,80 @@ for (const btn of projectsBtns) {
 		projectsBlock.classList.add('active');
 	});
 }
+
+
+
+//Trying add swiper slider instead of static structure
+//Changes in html projects__block
+
+fetch("js/projectsCommercial.json")
+	.then((response) => response.json())
+	.then((data) => {
+		let slides = '';
+
+		for (let project of data) {
+			slides += `
+				<div class="swiper-slide projects__slide projects__block" data-index="1">
+					<div class="projects__card">
+						<img class="projects__image" src="${project.image}"
+							alt="Example of constructed building">
+						<div class="projects__info">
+							<p>${project.name}</p>
+							<p>${project.location}</p>
+						</div>
+					</div>
+				</div>
+			`;
+		}
+
+		document.querySelector('.projects__slider1').innerHTML = `
+			<div class="swiper-wrapper projects__wrapper">
+				${slides}
+		  	</div>
+		  	<!--  -->
+		  	<div class="swiper-pagination"></div>
+		  	<!-- <div class="swiper-button-prev"></div> -->
+		  	<!-- <div class="swiper-button-next"></div> -->
+		`;
+
+		const swiper1 = new Swiper('.projects__slider1', {
+			loop: true,
+
+			slidesPerView: 2,
+			watchOverflow: true,
+			spaceBetween: 125,
+			slidesPerGroup: 1,
+			centeredSlides: true,
+
+			effect: 'coverflow',
+			coverflowEffect: {
+				rotate: 0,
+				slideShadows: false,
+				scale: 1.0,
+			},
+			slideToClickedSlide: true,
+
+			keyboard: {
+				enabled: true,
+				onlyInViewport: true,
+				pageUpDown: true,
+			},
+
+			mousewheel: {
+				sensitivity: 1,
+				//eventTarget: ".testimonials__slider",
+			},
+
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			navigation: {
+				nextEl: '.projects__button-next',
+				prevEl: '.projects__button-prev',
+			},
+		});
+	})
+	.catch((error) => {
+		document.querySelector('.projects__slider1').innerHTML = '<p>Sorry, we have some problems. We will fix them soon.</p>';
+	});
