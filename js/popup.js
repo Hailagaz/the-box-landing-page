2a4558318f6af3cb00b4308312d62053b71007f7
Hailagaz <omentielvo.lumenn@gmail.com>
2022-11-07 18:52:01 +0200

    Fix js popup window

--- js/popup.js
+++ js/popup.js
@@ -104,3 +104,27 @@ document.addEventListener('keydown', function (e) {
 		popupClose(popupActive);
 	}
 });
+
+
+// Polyfills
+(function () {
+	if (!Element.prototype.closest) {
+		Element.prototype.closest = function (css) {
+			var node = this;
+			while (node) {
+				if (node.matches(css)) return node;
+				else node = node.parentElement;
+			}
+			return null;
+		};
+	}
+})();
+
+(function () {
+	if (!Element.prototype.matches) {
+		Element.prototype.matches = Element.prototype.matchesSelector || 
+			Element.prototype.webkitMatchesSelector || 
+			Element.prototype.mozMatchesSelector || 
+			Element.prototype.nsMatchesSelector;
+	}
+})();
