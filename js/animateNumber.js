cb826e616973f7c09da2f8c1c76b239ee5befaca
Hailagaz <omentielvo.lumenn@gmail.com>
2022-11-10 19:12:16 +0200

    Fix section stats (in progress)

--- js/animateNumber.js
+++ js/animateNumber.js
@@ -1,6 +1,16 @@
 let valueDisplays = document.querySelectorAll(".stats__num");
 let interval = 5000;
 
+
+//let posStats = document.getElementById("stats").scrollHeight;
+// if (posStats > "735") {
+
+// window.addEventListener("scroll", function (e) {
+// 	if (window.scrollY > 700) {}}
+
+
+
+
 valueDisplays.forEach((valueDisplay) => {
 	let startValue = 0;
 	let endValue = parseInt(valueDisplay.getAttribute("data-val"));
