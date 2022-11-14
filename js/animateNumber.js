0c9ae2ec62f6b5a369c4df395d4ce3a838df6d29
Hailagaz <omentielvo.lumenn@gmail.com>
2022-11-14 12:08:15 +0200

    Fix stats section js counting

--- js/animateNumber.js
+++ js/animateNumber.js
@@ -1,26 +1,57 @@
-let valueDisplays = document.querySelectorAll(".stats__num");
-let interval = 5000;
+// let valueDisplays = document.querySelectorAll(".stats__num");
+// let interval = 5000;
+
+// valueDisplays.forEach((valueDisplay) => {
+// 	let startValue = 0;
+// 	let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+// 	let duration = Math.floor(interval / endValue);
+
+// 	let counter = setInterval(function () {
+// 		startValue += 1;
+// 		valueDisplay.textContent = startValue;
+// 		if (startValue == endValue) {
+// 			clearInterval(counter);
+// 		}
+// 	}, duration);
+// });
 
 
-//let posStats = document.getElementById("stats").scrollHeight;
-// if (posStats > "735") {
-
-// window.addEventListener("scroll", function (e) {
-// 	if (window.scrollY > 700) {}}
 
 
 
+//Version 2
 
-valueDisplays.forEach((valueDisplay) => {
-	let startValue = 0;
-	let endValue = parseInt(valueDisplay.getAttribute("data-val"));
-	let duration = Math.floor(interval / endValue);
+//page__section -> stats
+//data-digits-counter -> data-val
+//page__counter -> stats__counter
 
-	let counter = setInterval(function () {
-		startValue += 1;
-		valueDisplay.textContent = startValue;
-		if (startValue == endValue) {
-			clearInterval(counter);
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+
+	function digitsCountersInit(digitsCountersItems) {
+		let digitsCounters = digitsCountersItems ? digitsCountersItems : document.querySelectorAll("[data-digits-counter]");
+		if (digitsCounters) {
+			digitsCounters.forEach(digitsCounter => {
+				digitsCountersAnimate(digitsCounter);
+			});
 		}
-	}, duration);
-});
+	}
+
+	function digitsCountersAnimate(digitsCounter) {
+		let startTimestamp = null;
+		const duration = parseInt(digitsCounter.dataset.digitsCounter) ? parseInt(digitsCounter.dataset.digitsCounte) : 1000;
+		const startValue = parseInt(digitsCounter.innerHTML);
+		const startPosition = 0;
+		const step = (timestamp) => {
+			if (!startTimestamp) startTimestamp = timestamp;
+			const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+			digitsCounter.innerHTML = Math.floor(progress * (startPosition + startValue));
+			if (progress < 1) window.requestAnimationFrame(step);
+		};
+		window.requestAnimationFrame(step);
+	}
+
+	//Start when page loaded
+	//digitsCountersInit();
+}
