bbcbaebeaad7605f603ee29d3869d255717fb724
Hailagaz <omentielvo.lumenn@gmail.com>
2022-11-15 09:28:27 +0200

    Fix section hero

--- js/sectionHero.js
+++ js/sectionHero.js
@@ -1,11 +1,12 @@
 //Get data and build sliders structure
-fetch("js/featureProjects.json")
-	.then((response) => response.json())
-	.then((data) => {
-		let slides = '';
+document.addEventListener("DOMContentLoaded", () => {
+	fetch("js/featureProjects.json")
+		.then((response) => response.json())
+		.then((data) => {
+			let slides = '';
 
-		for (let project of data) {
-			slides += `
+			for (let project of data) {
+				slides += `
 				<div class="swiper-slide hero__slide">
 					<div class="hero__photo">
 						<img src="${project.photo}" alt="Photo of build project">
@@ -29,9 +30,9 @@ fetch("js/featureProjects.json")
 					</div>
 				</div>
 			`;
-		}
+			}
 
-		document.querySelector('.swiper').innerHTML = `
+			document.querySelector('.swiper').innerHTML = `
 		<div class="swiper-wrapper hero__wrapper">
 			${slides}
 		  </div>
@@ -41,44 +42,46 @@ fetch("js/featureProjects.json")
 		  <!-- <div class="swiper-button-next"></div> -->
 	`;
 
-		const swiper = new Swiper('.swiper', {
-			loop: true,
+			const swiper = new Swiper('.swiper', {
+				loop: true,
 
-			slidesPerView: 1,
-			watchOverflow: true,
-			spaceBetween: 125,
-			slidesPerGroup: 1,
-			centeredSlides: true,
+				slidesPerView: 1,
+				watchOverflow: true,
+				spaceBetween: 125,
+				slidesPerGroup: 1,
+				centeredSlides: true,
 
-			effect: 'coverflow',
-			coverflowEffect: {
-				rotate: 0,
-				slideShadows: false,
-				scale: 1.0,
-			},
-			slideToClickedSlide: true,
+				effect: 'coverflow',
+				coverflowEffect: {
+					rotate: 0,
+					slideShadows: false,
+					scale: 1.0,
+				},
+				slideToClickedSlide: true,
 
-			keyboard: {
-				enabled: true,
-				onlyInViewport: true,
-				pageUpDown: true,
-			},
+				keyboard: {
+					enabled: true,
+					onlyInViewport: true,
+					pageUpDown: true,
+				},
 
-			mousewheel: {
-				sensitivity: 1,
-				//eventTarget: ".testimonials__slider",
-			},
+				mousewheel: {
+					sensitivity: 1,
+					//eventTarget: ".testimonials__slider",
+				},
 
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			navigation: {
-				nextEl: '.hero__button-next',
-				prevEl: '.hero__button-prev',
-			},
+				pagination: {
+					el: '.swiper-pagination',
+					clickable: true,
+				},
+				navigation: {
+					nextEl: '.hero__button-next',
+					prevEl: '.hero__button-prev',
+				},
+			});
+		})
+		.catch((error) => {
+			document.querySelector('.swiper').innerHTML = '<p>Sorry, we have some problems. We will fix them soon.</p>';
 		});
-	})
-	.catch((error) => {
-		document.querySelector('.swiper').innerHTML = '<p>Sorry, we have some problems. We will fix them soon.</p>';
-	});
+});
+
